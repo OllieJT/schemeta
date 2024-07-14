08e49387fe395995ed1cd6e6e38b49501cb38dd4
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2024-07-14 13:22:44 +0100

    Fix publint

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,6 +5,9 @@ const is_dev = process.env.npm_lifecycle_event === "dev";
 export default defineConfig((options) => {
 	const debug = Boolean(options.watch) || is_dev;
 
+	if (debug) console.warn("🚧 Building in debug mode");
+	else console.warn("🏗️ Building in production mode");
+
 	return {
 		clean: true,
 		entry: ["src/index.ts", "src/metadata.ts", "src/transform.ts", "src/use.ts"],
