77b636dba1e3c96e7b87136efbdf7311dcd960b2
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2024-07-14 19:34:01 +0100

    docs: Adds basic documentation (#5)
    * bump tooling versions
    
    * Simplify og:type metadata
    
    * Adds support for Linked Data
    
    * Fix formatting
    
    * Minor lib refinements
    
    * Added basic docs

---
+++ .changeset/hungry-rabbits-confess.md
@@ -0,0 +1,5 @@
+---
+"schemeta": patch
+---
+
+Added basic docs
--- src/use.locale.ts
+++ src/use.locale.ts
@@ -1,6 +1,6 @@
 import type { MetadataElement, OpenGraph } from "./types/tags.js";
 
-export const use_locale = (site: { locale: string; locale_alternate?: string[] }) => {
+export const use_locale = (site: { locale: string; locale_alt?: string[] }) => {
 	const elements: MetadataElement[] = [
 		{
 			element: "meta",
@@ -8,8 +8,8 @@ export const use_locale = (site: { locale: string; locale_alternate?: string[] }
 		} satisfies OpenGraph.Locale,
 	];
 
-	if (site.locale_alternate) {
-		site.locale_alternate.forEach((locale) => {
+	if (site.locale_alt) {
+		site.locale_alt.forEach((locale) => {
 			const og_locale_alt: OpenGraph.LocaleAlternate = {
 				element: "meta",
 				attributes: { property: "og:locale:alternate", content: locale },
