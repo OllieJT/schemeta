d32c3ddfe5b111036505172beb80ef3e4497680c
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2024-07-14 13:14:55 +0100

    Remove eslint type

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import prettier from "eslint-config-prettier";
 import globals from "globals";
 import ts from "typescript-eslint";
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
 	js.configs.recommended,
 	...ts.configs.recommended,
